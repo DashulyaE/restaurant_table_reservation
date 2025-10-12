8dbbe7c44d0b94474972f4d1ec7eb71d64d2c6fd
Darya <dasha-439@yandex.ru>
2025-10-12 21:15:20 +0300

    Переопределены стили bootstrap

--- reservations/views.py
+++ reservations/views.py
@@ -6,4 +6,4 @@ from reservations.models import Restaurant
 def restaurants_list(request):
     restaurants = Restaurant.objects.all()
     context = {"restaurants": restaurants}
-    return render(request, "reservations/base.html", context)
+    return render(request, "reservations/restaurant_list.html", context)
