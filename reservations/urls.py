9435e42cf876938874a2029fc4041f774515e52a
Darya <dasha-439@yandex.ru>
2025-10-12 13:54:02 +0300

    Начальные настройки

---
+++ reservations/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from reservations.apps import ReservationsConfig
+from reservations.views import index
+
+app_name = ReservationsConfig.name
+
+urlpatterns = [
+    path('', index)
+]
