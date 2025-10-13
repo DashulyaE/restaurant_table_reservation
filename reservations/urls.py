17e27cab402bd866fe2c38af90ea98ef7e547b8b
Darya <dasha-439@yandex.ru>
2025-10-13 12:54:43 +0300

    Настроены шаблоны перехода

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 
 from config import settings
 from reservations.apps import ReservationsConfig
-from reservations.views import restaurants_list
+from reservations.views import restaurants_list, restaurants_detail
 
 app_name = ReservationsConfig.name
 
 urlpatterns = [
-    path("", restaurants_list),
+    path("", restaurants_list, name='restaurants_list'),
+    path("restaurants/<int:pk>/", restaurants_detail, name='restaurants_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reservations/views.py
+++ reservations/views.py
@@ -7,3 +7,9 @@ def restaurants_list(request):
     restaurants = Restaurant.objects.all()
     context = {"restaurants": restaurants}
     return render(request, "reservations/restaurant_list.html", context)
+
+
+def restaurants_detail(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    context = {"restaurant": restaurant}
+    return render(request, "reservations/restaurants_detail.html", context)
