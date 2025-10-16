636cda5344ff097a3c824c29378f1b498d5c2b6a
Darya <dasha-439@yandex.ru>
2025-10-16 23:08:43 +0300

    Верификация

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 from users.apps import UsersConfig
-from users.views import RegisterView
+from users.views import RegisterView, email_verification
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('register/', RegisterView.as_view(), name='register'),
+    path('email-confirm/<str:token>/', email_verification, name="email-confirm"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
+import secrets
 from django.contrib.auth import login
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from .forms import UserRegisterForm
 from .models import User
 
@@ -12,14 +15,26 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
 
-    # def form_valid(self, form):
-    #     user = form.save()
-    #     login(self.request, user)
-    #     self.send_welcome_email(user.email)
-    #     return super().form_valid(form)
-    #
-    # def send_welcome_email(self, user_email):
-    #     subject = 'Добро пожаловать в наш сервис'
-    #     message = 'Спасибо, что зарегистрировались в нашем сервисе!'
-    #     recipient_list = [user_email]
-    #     send_mail(subject, message, EMAIL_HOST_USER, recipient_list)
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}/"
+        send_mail(
+            subject="Подтверждение почты на сайте РезервСтолик'",
+            message=f"Благодарим Вас за регистраницию на сайте РезервСтолик. Перейдите по ссылке для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+def email_verification(request, token):
+    """Подтверждение почты"""
+
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
