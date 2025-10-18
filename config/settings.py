1a6d1dc13cbb40a1860637e861676e2d8958d36a
Darya <dasha-439@yandex.ru>
2025-10-18 23:01:21 +0300

    При регистрации пользователя он сразу добавляется в группу user

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "reservations",
-    "users",
+    "users.apps.UsersConfig",
     "phonenumber_field",
 ]
 
